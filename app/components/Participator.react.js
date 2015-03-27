36bc40d32eec5fc6c5b5f2b02202f798ac2824a2
zhudaye <1607291079@qq.com>
2015-03-27 19:28:34 +0800

    fix some bugs

---
+++ app/components/Participator.react.js
@@ -0,0 +1,19 @@
+/**
+ * Participator React Componment
+ * @author SurfaceW
+ * @version 1.0 
+ */
+
+var React           = window.React;
+var $               = window.$;
+
+var Participator = React.createClass({
+
+	render: function () {
+		return (
+			<div>Hello, World!</div>
+		);
+	}
+});
+
+module.exports = Participator;
